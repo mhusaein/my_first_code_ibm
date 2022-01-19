ce62705452e7478345957d3d34f7efa41610e1b5
minmohxo <71086157+minmohxo@users.noreply.github.com>
2022-01-19 22:48:13 +0200

    Add files via upload

---
+++ my first code.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Mohamed Hussein**
+# 
+# Production Engineer
+
+# *I'm interested in data analysis and data science, being able to get findings based on data collection that can help make desicions for future developments*
+
+# ### This code add 25 to any number
+
+# In[1]:
+
+
+def add_25(n):
+    return n+25
+add_25(0)
+
+
+# this is an unordered list : 
+# * a
+# * b
+# * c
+# 
+# this is an ordered list : 
+# 1. a
+# 2. b
+# 3. c
+# ***
